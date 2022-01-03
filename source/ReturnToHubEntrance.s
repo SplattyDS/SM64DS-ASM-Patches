548f8c9df768bd703917df2747b7ee6c8d33a408
Splatterboy64 <janssensdylian@gmail.com>
2022-01-03 19:34:54 +0100

    Optimise second hook in ReturnToHubEntrance.s

--- source/ReturnToHubEntrance.s
+++ source/ReturnToHubEntrance.s
@@ -9,25 +9,22 @@ nsub_02029378:
 	ldrb r0, [r0]
 	ldr r1, =HUB_ENTRANCE_ID
 	ldrb r1, [r1]
-	b 0x0202937c @   go to the next instruction
+	b 0x0202937c 					@ Go to the next instruction
 
 @ Hook when entering exit, save HUB entrance and level here if level hub
 @ Safe: r0 and r1
 @ NEXT_ENTRANCE_ID: r3
 nsub_0202AB68:
 	@ Set hub level and entrance ids if warping to a non-hub level from the hub
+	push {r14}
 	ldr r0, =0x02092110				@ Load address of NEXT_LEVEL_ID in r0
 	ldrb r0, [r0]					@ Load value   of NEXT_LEVEL_ID in r0
-	push {r14}
 	bl 0x02013558					@ Load ACT_SELECTOR_ID of NEXT_LEVEL_ID in r0: SubLevelToLevel(r0)
-	pop {r14}
 	cmp r0, #0x1D					@ NEXT_ACT_SELECTOR_ID == 0x1D (29: HUB)
 	beq exit_code_0202AB68			@ If true, exit code
 	ldr r0, =0x0209f2f8				@ Load address of LEVEL_ID in r0
 	ldrb r0, [r0]					@ Load value of LEVEL_ID in r0
-	push {r14}
 	bl 0x02013558					@ Load ACT_SELECTOR_ID of LEVEL_ID in r0: SubLevelToLevel(r0)
-	pop {r14}
 	cmp r0, #0x1D					@ ACT_SELECTOR_ID == 0x1D (29: HUB)
 	bne exit_code_0202AB68			@ If false, exit code
 	
@@ -41,5 +38,6 @@ nsub_0202AB68:
 	b exit_code_0202AB68			@ Exit code
 	
 	exit_code_0202AB68:
+	pop {r14}
 	ldr r1, =0x0209f2f8				@ Execute original instruction
 	b 0x0202ab6c					@ Go to the next instruction
