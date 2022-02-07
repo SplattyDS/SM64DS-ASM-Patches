33402c8ae9bf604c2b691bfefbfd6db98634387e
Dylian Janssens <71971638+Splatterboy64@users.noreply.github.com>
2022-02-07 23:16:59 +0100

    Update ChangeStarRotationSpeed.s with comment

--- source/ChangeStarRotationSpeed.s
+++ source/ChangeStarRotationSpeed.s
@@ -1,3 +1,3 @@
 nsub_020e8578_ov_02:
-	add r1, r1, 0x0c00	@ higher = faster (default: 0x0c00)
+	add r1, r1, 0x0c00	@ higher = faster (default: 0x0c00), change "add" to "sub" if you want the star to spin in the opposite direction
 	b 0x020e857c
