463e0bed5b49765135939d9a042bdb84c7092c50
Dylian Janssens <71971638+Splatterboy64@users.noreply.github.com>
2022-04-21 18:42:56 +0200

    Optimize StayInLevelAfterStar.s

--- source/StayInLevelAfterStar.s
+++ source/StayInLevelAfterStar.s
@@ -1,6 +1,4 @@
 @ Stay in level after collecting a star (credits to dy for the address and overlay)
-nsub_020E742C_ov_02:
-	b 0x020E7430
+nsub_020E742C_ov_02 = 0x020E7430
 
-nsub_020E9678_ov_02:
-	b 0x020E967C
+nsub_020E9678_ov_02 = 0x020E967C
